983f03327ab8822f359fbb25c7f36373e131d79f
KRISHNA KUMAR VERMA <155811536+wd369018@users.noreply.github.com>
2024-08-25 00:25:43 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,54 @@
+# Calculator Program
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero is not allowed."
+    else:
+        return x / y
+
+def calculator():
+    print("Simple Calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter your choice (1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Invalid choice")
+
+def main():
+    calculator()
+    again = input("Do you want to calculate again? (yes/no): ")
+    if again.lower() == 'yes':
+        main()
+    else:
+        print("Goodbye!")
+
+if __name__ == "__main__":
+    main()
